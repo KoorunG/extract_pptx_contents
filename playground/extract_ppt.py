363d592c76d9ff988233ed168406295e3c0272f1
KoorunG <onlyzion10@naver.com>
2024-01-05 01:59:03 +0900

    refactor: PPT_텍스트 추출 함수 리팩토링

--- playground/extract_ppt.py
+++ playground/extract_ppt.py
@@ -1,88 +1,29 @@
-import re
-from pathlib import Path
-
+from datetime import datetime
 from openpyxl import Workbook
-from openpyxl.cell import Cell
-from pptx import Presentation
-from pptx.slide import Slide
-
 from playground.functions import *
-
-# 루트 경로
-root: Path = Path('/Users/koorung/Desktop/project_folder/')
-
-# 엑셀 파일명
-excel_name: str = '텍스트추출.xlsx'
-
-# # PPT 파일명
-# ppt_name: str = '제안본문_Ⅱ.전략및방법론_20240103_이재학_본문점검 크로스체크_v0.09.pptx'
-# # 경로
-# p: Path = root / ppt_name
-# # 프레젠테이션
-# prs: Presentation = Presentation(p)
-# sls: list[Slide] = prs.slides
 
 # 워크북
 wb = Workbook()
 ws = wb.active
-
 # 헤더 추가
 ws.append(['인덱스', 'PPT 파일명', '슬라이드 번호', '추출텍스트'])
 
 # 전역 번호
 rn: int = 1
 
+# 루트 경로
+root: Path = Path('C:/Users/jaehak/Desktop/회사/00. 제안작업/1. 본문/★ 본문점검/☆ 본문점검 제안요청서 매칭/추출대상 PPT')
+
+# 루트 하위의 .pptx 파일을 찾아서 ppt 읽기
 for i in root.iterdir():
     if i.is_file() and i.suffix == ".pptx":
-        print(i)
-        prs: Presentation = Presentation(i)
-        sls: list[Slide] = prs.slides
-        for index, sl in enumerate(sls):
-            # 중복제거용 set()
-            lines: set[str] = set()
-
-            # 슬라이드별 텍스트 추출하기
-            extract_text_in_ppt(sl.shapes, lines)
-
-            # 콘솔에 찍기 (검증용)
-            print()
-            print(f'******** {index}번째 슬라이드 ********')
-            print(lines)
-
-            # 특수문자만 있는 경우를 제외하고 엑셀에 추가
-            for line in lines:
-                regex = re.compile("[0-9a-zA-Zㄱ-힗]", re.MULTILINE)
-                if re.match(regex, line):
-                    ws.append([rn, i.name, str(index) + "번째", line])
-                    rn += 1
+        read_ppt(rn, i, ws)
 
 # 디자인
-tups: list[tuple[Cell]] = ws.rows
+design_excel(ws)
 
-for i, tup in enumerate(tups):
-    a, b, c, d = tup
-
-    if i == 0:
-        a.fill = fill()
-        b.fill = fill()
-        c.fill = fill()
-        d.fill = fill()
-        a.font = font('KoPubDotum', 'ffffff', 13)
-        b.font = font('KoPubDotum', 'ffffff', 13)
-        c.font = font('KoPubDotum', 'ffffff', 13)
-        d.font = font('KoPubDotum', 'ffffff', 13)
-    else:
-        a.font = font('KoPubDotum')
-        b.font = font('KoPubDotum')
-        c.font = font('KoPubDotum')
-        d.font = font('KoPubDotum')
-
-    a.border = border()
-    b.border = border()
-    c.border = border()
-    d.border = border()
-
-auto_fit_column_size(ws, margin=10)
+# 엑셀 파일명
+excel_name: str = f'텍스트추출_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx'
 
 # 저장
 wb.save(root / excel_name)
--- playground/functions.py
+++ playground/functions.py
@@ -1,16 +1,26 @@
+import re
+
+from openpyxl.cell import Cell
 from openpyxl.styles import Font, Border, PatternFill
 from openpyxl.styles import Side
+from openpyxl.worksheet.worksheet import Worksheet
+from pptx import Presentation
 from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pptx.slide import Slide
+from pathlib import Path
 
 
 # PPT에서 텍스트 추출하기
-def extract_text_in_ppt(shapes, texts: set[str]):
+def extract_text_in_ppt(shapes) -> set[str]:
+    # 중복제거용 set()
+    lines: set[str] = set()
+
     for shape in shapes:
         # 1. 텍스트 프레임인 경우
         if shape.has_text_frame:
             for paragraph in shape.text_frame.paragraphs:
                 for run in paragraph.runs:
-                    texts.add(run.text)
+                    lines.add(run.text)
                     # print(run.text, end="")
         # 2. 표인 경우
         elif shape.has_table:
@@ -18,12 +28,14 @@ def extract_text_in_ppt(shapes, texts: set[str]):
                 for cell in row.cells:
                     for cell_paragraph in cell.text_frame.paragraphs:
                         for cell_run in cell_paragraph.runs:
-                            texts.add(cell_run.text)
+                            lines.add(cell_run.text)
                             # print(cell_run.text, end="")
 
         # 3. 그룹인경우 재귀
         elif shape.shape_type == MSO_SHAPE_TYPE.GROUP:
-            extract_text_in_ppt(shape.shapes, texts)
+            extract_text_in_ppt(shape.shapes)
+
+    return lines
 
 
 def auto_fit_column_size(worksheet, columns=None, margin=2):
@@ -41,16 +53,47 @@ def auto_fit_column_size(worksheet, columns=None, margin=2):
     return worksheet
 
 
-def border(border_style='thin', color='000000'):
-    return Border(left=Side(border_style, color),
-                  right=Side(border_style, color),
-                  top=Side(border_style, color),
-                  bottom=Side(border_style, color))
+def base_border(border_style='thin', rgb='000000'):
+    return Border(left=Side(border_style, rgb),
+                  right=Side(border_style, rgb),
+                  top=Side(border_style, rgb),
+                  bottom=Side(border_style, rgb))
 
 
-def font(name, color='000000', size=11):
-    return Font(name=name, color=color, size=size)
+def base_font(name, rgb, size):
+    return Font(name=name, color=rgb, size=size)
 
 
-def fill():
-    return PatternFill(start_color='000000', end_color='000000', fill_type='solid')
+def base_fill(rgb: str):
+    return PatternFill(start_color=rgb, end_color=rgb, fill_type='solid')
+
+
+def design_excel(ws: Worksheet):
+    rows: list[tuple[Cell]] = ws.rows
+
+    # 폰트설정
+    font_style: str = 'KoPub돋움체 Medium'
+
+    for i, tup in enumerate(rows):
+        for el in tup:
+            el.fill = base_fill('000000') if i == 0 else base_fill('ffffff')
+            el.font = base_font(font_style, 'ffffff', 13) if i == 0 else base_font(font_style, '000000', 11)
+            el.border = base_border()
+    auto_fit_column_size(ws, margin=10)
+
+
+def read_ppt(n: int, path: Path, ws: Worksheet):
+    prs: Presentation = Presentation(path)
+    sls: list[Slide] = prs.slides
+    for i, sl in enumerate(sls):
+        print(f'[{path.name}] :: {i}번째 슬라이드 읽음')
+        
+        # 슬라이드별 텍스트 추출하기 & 특수문자만 있는 경우를 제외하고 엑셀에 추가
+        [append_row(i, n, text, path.name, ws) for text in extract_text_in_ppt(sl.shapes)]
+
+
+def append_row(slide_index: int, index: int, text: str, file_name: str, ws: Worksheet):
+    regex = re.compile("[0-9a-zA-Zㄱ-힗]", re.MULTILINE)
+    if re.match(regex, text):
+        ws.append([index, file_name, str(slide_index) + "번째", text])
+        index += 1
