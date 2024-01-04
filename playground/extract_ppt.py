a4ffb1c373fe5f64bf91843a9e64627c40b705ed
KoorunG <onlyzion10@naver.com>
2024-01-05 03:07:02 +0900

    refactor: 인덱스 리팩토링

--- playground/extract_ppt.py
+++ playground/extract_ppt.py
@@ -8,16 +8,13 @@ ws = wb.active
 # 헤더 추가
 ws.append(['인덱스', 'PPT 파일명', '슬라이드 번호', '추출텍스트'])
 
-# 전역 번호
-rn: int = 1
-
 # 루트 경로
 root: Path = Path('C:/Users/jaehak/Desktop/회사/00. 제안작업/1. 본문/★ 본문점검/☆ 본문점검 제안요청서 매칭/추출대상 PPT')
 
 # 루트 하위의 .pptx 파일을 찾아서 ppt 읽기
 for i in root.iterdir():
     if i.is_file() and i.suffix == ".pptx":
-        read_ppt(rn, i, ws)
+        read_ppt(i, ws)
 
 # 디자인
 design_excel(ws)
--- playground/functions.py
+++ playground/functions.py
@@ -9,7 +9,6 @@ from pptx.enum.shapes import MSO_SHAPE_TYPE
 from pptx.slide import Slide
 from pathlib import Path
 
-
 # PPT에서 텍스트 추출하기
 def extract_text_in_ppt(shapes) -> set[str]:
     # 중복제거용 set()
@@ -18,24 +17,27 @@ def extract_text_in_ppt(shapes) -> set[str]:
     for shape in shapes:
         # 1. 텍스트 프레임인 경우
         if shape.has_text_frame:
-            for paragraph in shape.text_frame.paragraphs:
-                for run in paragraph.runs:
-                    lines.add(run.text)
-                    # print(run.text, end="")
+            add_line_from_paragraphs(lines, shape.text_frame.paragraphs)
         # 2. 표인 경우
         elif shape.has_table:
-            for row in shape.table.rows:
-                for cell in row.cells:
-                    for cell_paragraph in cell.text_frame.paragraphs:
-                        for cell_run in cell_paragraph.runs:
-                            lines.add(cell_run.text)
-                            # print(cell_run.text, end="")
-
+            row_generator = (row for row in shape.table.rows)
+            for row in row_generator:
+                cell_generator = (cell for cell in row.cells)
+                for cell in cell_generator:
+                    add_line_from_paragraphs(lines, cell.text_frame.paragraphs)
         # 3. 그룹인경우 재귀
         elif shape.shape_type == MSO_SHAPE_TYPE.GROUP:
             extract_text_in_ppt(shape.shapes)
 
     return lines
+
+
+def add_line_from_paragraphs(lines, paragraphs):
+    paragraph_generator = (paragraph for paragraph in paragraphs)
+    for paragraph in paragraph_generator:
+        run_generator = (run for run in paragraph.runs)
+        for run in run_generator:
+            lines.add(run.text)
 
 
 def auto_fit_column_size(worksheet, columns=None, margin=2):
@@ -82,18 +84,23 @@ def design_excel(ws: Worksheet):
     auto_fit_column_size(ws, margin=10)
 
 
-def read_ppt(n: int, path: Path, ws: Worksheet):
+def read_ppt(path: Path, ws: Worksheet):
     prs: Presentation = Presentation(path)
     sls: list[Slide] = prs.slides
     for i, sl in enumerate(sls):
         print(f'[{path.name}] :: {i}번째 슬라이드 읽음')
         
         # 슬라이드별 텍스트 추출하기 & 특수문자만 있는 경우를 제외하고 엑셀에 추가
-        [append_row(i, n, text, path.name, ws) for text in extract_text_in_ppt(sl.shapes)]
+        text_generator = (text for text in extract_text_in_ppt(sl.shapes))
+        for text in text_generator:
+            append_row(i, text, path.name, ws)
 
+global i
+i = 1
 
-def append_row(slide_index: int, index: int, text: str, file_name: str, ws: Worksheet):
+def append_row(slide_index: int, text: str, file_name: str, ws: Worksheet):
+    global i
     regex = re.compile("[0-9a-zA-Zㄱ-힗]", re.MULTILINE)
     if re.match(regex, text):
-        ws.append([index, file_name, str(slide_index) + "번째", text])
-        index += 1
+        ws.append([i, file_name, str(slide_index) + "번째", text])
+        i += 1
