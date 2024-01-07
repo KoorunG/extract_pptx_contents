5cac80312ea367b1277f5d13deab0804bfa35bb0
KoorunGG <onlyzion10@naver.com>
2024-01-08 00:44:45 +0900

    pr: test_extract.py 테스트 파일 추가

--- playground/excel_design.py
+++ playground/excel_design.py
@@ -12,13 +12,13 @@ def design_excel(ws: Worksheet):
 
     for i, tup in enumerate(rows):
         for el in tup:
-            el.fill = _base_fill('000000') if i == 0 else _base_fill('ffffff')
-            el.font = _base_font(font_style, 'ffffff', 13) if i == 0 else _base_font(font_style, '000000', 11)
-            el.border = _base_border()
-    auto_fit_column_size(ws, margin=10)
+            el.fill = __base_fill('000000') if i == 0 else __base_fill('ffffff')
+            el.font = __base_font(font_style, 'ffffff', 13) if i == 0 else __base_font(font_style, '000000', 11)
+            el.border = __base_border()
+    __auto_fit_column_size(ws, margin=10)
 
 
-def auto_fit_column_size(worksheet, columns=None, margin=2):
+def __auto_fit_column_size(worksheet, columns=None, margin=2):
     for i, column_cells in enumerate(worksheet.columns):
         is_ok = False
         if columns is None or (isinstance(columns, list) and i in columns):
@@ -31,16 +31,16 @@ def auto_fit_column_size(worksheet, columns=None, margin=2):
     return worksheet
 
 
-def _base_border(border_style='thin', rgb='000000'):
+def __base_border(border_style='thin', rgb='000000'):
     return Border(left=Side(border_style, rgb),
                   right=Side(border_style, rgb),
                   top=Side(border_style, rgb),
                   bottom=Side(border_style, rgb))
 
 
-def _base_font(name, rgb, size):
+def __base_font(name, rgb, size):
     return Font(name=name, color=rgb, size=size)
 
 
-def _base_fill(rgb: str):
+def __base_fill(rgb: str):
     return PatternFill(start_color=rgb, end_color=rgb, fill_type='solid')
---
+++ playground/test_extract.py
@@ -0,0 +1,35 @@
+import unittest
+from pathlib import Path
+
+from openpyxl import Workbook
+
+from ppt_extract import read_ppt
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        import_dir = Path().cwd() / '..'
+        export_dir = Path().cwd() / '..'
+
+        wb = Workbook()
+        ws = Workbook().create_sheet("test")
+
+        for i in import_dir.iterdir():
+            if i.is_file() and i.suffix == ".pptx":
+                read_ppt(i, ws)
+
+        wb.save(export_dir / 'testexcel.xlsx')
+
+        textexcel = export_dir / 'textexcel.xlsx'
+
+        self.assertIsNotNone(textexcel)
+        self.addCleanup(remove_file)
+
+
+def remove_file():
+    saved_file = Path().cwd() / '..' / 'testexcel.xlsx'
+    saved_file.unlink()
+
+
+if __name__ == '__main__':
+    unittest.main()
