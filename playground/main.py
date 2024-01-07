765f962a82975aafcb1e7fb8d619fd53b3fd7d5f
KoorunGG <onlyzion10@naver.com>
2024-01-08 00:12:32 +0900

    pr: 엑셀 파일명 f-string에서 일반 문자열로 변경

--- playground/main.py
+++ playground/main.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from pathlib import Path
+
 from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from pathlib import Path
+
 from excel_design import design_excel
 from ppt_extract import read_ppt
 
@@ -21,7 +23,6 @@ import_dir = root / '..' / 'import'
 # 2: .xls export 경로
 export_dir = root / '..' / 'export'
 
-
 # import_dir 하위의 .pptx 파일을 찾아서 읽기
 for i in import_dir.iterdir():
     if i.is_file() and i.suffix == ".pptx":
@@ -31,7 +32,7 @@ for i in import_dir.iterdir():
 design_excel(ws)
 
 # 엑셀 파일명
-excel_name: str = f'텍스트추출_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx'
+excel_name: str = '텍스트추출_' + datetime.now().strftime('%Y%m%d_%H%M%S') + '.xlsx'
 
 # 저장
 wb.save(export_dir / excel_name)
